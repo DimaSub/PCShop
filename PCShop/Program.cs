ecc4123f167642a7398c902acf1b5b92e31afeef
Dima Subotin <57070189+DimaSub@users.noreply.github.com>
2020-04-06 20:47:45 +0300

    First commit

---
+++ PCShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PCShop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
