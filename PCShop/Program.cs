fbbe7280891e23d6e7625591cdf89b268ef254ea
Dima Subotin <57070189+DimaSub@users.noreply.github.com>
2020-04-07 20:38:32 +0300

    first build

--- PCShop/Program.cs
+++ PCShop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PCShop
 {
@@ -6,7 +7,73 @@ namespace PCShop
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int exit = 0;
+            string menu = "Please input your option ->" +
+                          "\n\n1->Add new computer" +
+                          "\n2->Remove computer" +
+                          "\n3->Print computer list by Company Name" +
+                          "\n4->Print list of the computers" +
+                          "\n5->Calculate the total price of the computers" +
+                          "\n6->Еxit\n";
+
+            while (exit != 6)
+            {
+                Console.WriteLine(menu);
+                Console.Write("Please select an option: ");
+                string optionchk = Console.ReadLine();
+
+                while (!int.TryParse(optionchk, out int value))
+                {
+                    Console.Clear();
+                    Console.Write("\"{0}\" is not a valid option, please select again: \n\n",optionchk);
+                    Console.WriteLine(menu);
+                    Console.Write("Please select an option: ");
+                    optionchk = Console.ReadLine();
+                }
+
+                int option = int.Parse(optionchk);
+
+                if (option > 0 && option < 7)
+                {
+                    if (option == 1)
+                    {
+                        Console.Clear();
+                    }
+
+                    if (option == 2)
+                    {
+                        Console.Clear();
+                    }
+
+                    if (option == 3)
+                    {
+                        Console.Clear();
+                    }
+
+                    if (option == 4)
+                    {
+                        Console.Clear();
+                    }
+
+                    if (option == 5)
+                    {
+                        Console.Clear();
+                    }
+
+                    if (option == 6)
+                    {
+                        exit = option;
+                        Console.Clear();
+                        Console.WriteLine("Thank you for using our software");
+                    }
+                }
+
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\"{0}\" is not a valid option, please select again\n", option);
+                }
+            }
         }
     }
 }
